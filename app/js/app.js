241be18a0e427fb19bc2cc5cb59f2d74b0c672eb
gffny <gaffney.ie@gmail.com>
2014-06-03 08:37:57 -0400

    adding a controller, the app declaration, a profile template, and some basic data to the file

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,23 @@
 'use strict';
 
 /* App Module */
+var ldrbrdApp = angular.module('ldrbrdApp', [
+  'ngRoute',
+  'ldrbrdCtrls'
+]);
+
+ldrbrdApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/profile', {
+				templateUrl: 'templates/profile-detail.html',
+				controller: 'ProfileCtrl'
+			}).
+			when('/profile/:phoneId', {
+				templateUrl: 'templates/profile-detail.html',
+				controller: 'ProfileCtrl'
+			}).
+			otherwise({
+				redirectTo: '/home'
+			});
+		}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,11 @@
 'use strict';
 
 /* Controllers */
+var ldrbrdCtrls = angular.module('ldrbrdCtrls', []);
+
+ldrbrdCtrls.controller('ProfileCtrl', ['$scope', '$http',
+		function ($scope, $http) {
+			$http.get('data/profile.json').success(function(data) {
+				$scope.profile = data;
+			});
+		}]);
