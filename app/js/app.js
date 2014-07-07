ca7f9a3517c7bca5d559060e65d2a8da6ecb2053
gffny <gaffney.ie@gmail.com>
2014-07-07 12:49:59 -0400

    add detail to the application web app, including register, login, and leadboard screens and styles

--- app/js/app.js
+++ app/js/app.js
@@ -6,34 +6,89 @@ var ldrbrdApp = angular.module('ldrbrdApp', [
   'ldrbrdCtrls'
 ]);
 
+
+
+
 ldrbrdApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
+			/* Home */
+			when('/home', {
+				redirectTo: '/'
+			}).
+			when('/', {
+				templateUrl: 'app/templates/home.html',
+				controller: 'HomeCtrl'
+			}).
+			/* Login/Logout/Register */
+			when('/login', {
+				templateUrl: 'app/templates/auth/login.html',
+				controller: 'AuthCtrl'
+			}).
+			when('/logout', {
+				templateUrl: 'app/templates/auth/logout.html',
+				controller: 'AuthCtrl'
+			}).
+			when('/register', {
+				templateUrl: 'app/templates/auth/register.html',
+				controller: 'RegisterCtrl'
+			}).
+			/* Profile */
 			when('/profile', {
-				templateUrl: 'app/templates/profile-detail.html',
+				templateUrl: 'app/templates/profile/profile-detail.html',
 				controller: 'ProfileCtrl'
 			}).
-			when('/profile/:phoneId', {
-				templateUrl: 'app/templates/profile-detail.html',
+			when('/profile/:profileId', {
+				templateUrl: 'app/templates/profile/profile-detail.html',
 				controller: 'ProfileCtrl'
 			}).
+			/* Competition */
+			when('/competition/:competitionId', {
+				templateUrl: 'app/templates/competition/competition-detail.html',
+				controller: 'CompetitionCtrl'
+			}).
+			when('/competition/create', {
+				templateUrl: 'app/templates/competition/competition-create.html',
+				controller: 'CompetitionCtrl'
+			}).
+			when('/competition/results/:competitionId', {
+				templateUrl: 'app/templates/competition/competition-results.html',
+				controller: 'CompetitionCtrl'
+			}).
+			when('/competition/leaderboard/:competitionId', {
+				templateUrl: 'app/templates/competition/competition-leaderboard.html',
+				controller: 'LeaderboardCtrl'
+			}).
+			/* Courses */
 			when('/course', {
-				templateUrl: 'app/templates/course-overview.html',
+				templateUrl: 'app/templates/course/course-overview.html',
 				controller: 'CourseCtrl'
 			}).
 			when('/course/:courseId', {
-				templateUrl: 'app/templates/course-detail.html',
+				templateUrl: 'app/templates/course/course-detail.html',
 				controller: 'CourseCtrl'
 			}).
+			/* Scorecard */
 			when('/scorecard/', {
-				templateUrl: 'app/templates/scorecard-overview.html',
+				templateUrl: 'app/templates/scorecard/scorecard-overview.html',
 				controller: 'ScorecardCtrl'
 			}).
 			when('/scorecard/:scorecardId', {
-				templateUrl: 'app/templates/scorecard-detail.html',
+				templateUrl: 'app/templates/scorecard/scorecard-detail.html',
 				controller: 'ScorecardCtrl'
 			}).
+			/* Search */
+			when('search/:queryString', {
+				templateUrl: 'app/templates/search/search-results.html',
+				controller: 'SearchCtrl'
+			}).
+ 			/* Analysis */
+			when('/analysis/', {
+				templateUrl: 'app/templates/analysis/analysis-detail.html',
+				controller: 'CompetitionCtrl'
+			}).
+			/* Development Page */
 			otherwise({
-				redirectTo: '/home'
+				redirectTo: '/scorecard/a10af2db-d0ad-4f37-a906-f27aa5d8d35f'
 			});
 		}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,44 @@
 /* Controllers */
 var ldrbrdCtrls = angular.module('ldrbrdCtrls', []);
 
+/* Home */
+ldrbrdCtrls.controller('HomeCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
+
+		}]);
+
+/* Login/Logout/Register */
+ldrbrdCtrls.controller('AuthCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
+
+		}]);
+
+ldrbrdCtrls.controller('RegisterCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
+			var golfer = {
+				"firstName": "",
+				"lastName": "",
+				"profileName": "",
+				"city": "",
+				"state": "",
+				"handicap": ""
+			};
+
+			$scope.golfer = golfer;
+
+			$scope.register = function() {
+				console.log("register " + $scope.golfer);
+
+				$http.post("#/home", angular.toJson($scope.golfer)).success(function(data) {
+					console.log("success "+data);
+				}).error(function(data){
+					console.log("error "+data);
+				});
+			}
+
+		}]);
+
+/* Profile */
 ldrbrdCtrls.controller('ProfileCtrl', ['$scope', '$http',
 		function ($scope, $http) {
 
@@ -36,6 +74,63 @@ ldrbrdCtrls.controller('ProfileCtrl', ['$scope', '$http',
  				}
  			});
 		}]);
+
+/* Competition */
+ldrbrdCtrls.controller('CompetitionCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
+
+		}]);
+
+ldrbrdCtrls.controller('LeaderboardCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
+
+			$scope.leaderboard = [];
+
+			for(var i=0; i<10; i++) {
+				$scope.leaderboard[i] = {
+					"pos": i+1,
+					"name": "John Gaffney",
+					"thru": i+8,
+					"toPar": i-6
+				};
+			};
+
+		}]);
+
+/* Courses */
+ldrbrdCtrls.controller('CourseCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
+
+			$scope.abbreviatedView = true;
+			$scope.showHoleNote = false;
+			$scope.showMouseOverHoleView = false;
+			$scope.mohv = {};
+			$scope.tableClass=[];
+
+			// $scope.scorecard.course.id ... 7f500b91-cec5-4573-921a-ac2d3c2ca1af
+			$http.get('app/data/course-'+$routeParams.courseId+'.json').success(function(courseData) {
+				$scope.course = angular.copy(courseData.payload.courseHoleMap[$routeParams.courseId]);
+				// setup the mouse over 
+				for(var hole in $scope.course) {
+					$scope.tableClass[hole] = false;
+				}
+			});
+			//TODO HANDLE FAILUE HERE
+
+			$scope.toogleView = function() {
+				$scope.abbreviatedView = $scope.abbreviatedView ? false : true;
+				$scope.showMouseOverHoleView = false;
+			};
+
+			$scope.toggleHoleView = function(hole, column) {
+				$scope.mohv = hole;
+				$scope.mohv.holeImageURL = "http://lunar.thegamez.net/golfswing/youtube-golf-lessons/hole-10-bald-cypress-cheraw-golf-course-hole-by-hole-tour-300x506.jpg";
+				$scope.tableClass[column] = $scope.tableClass[column] ? false : true;
+				$scope.showMouseOverHoleView = $scope.showMouseOverHoleView ? false : true;
+			};
+		}]);
+
+/* Scorecard */
 ldrbrdCtrls.controller('ScorecardCtrl', ['$scope', '$http', '$routeParams',
 		function ($scope, $http, $routeParams) {
 
@@ -54,5 +149,17 @@ ldrbrdCtrls.controller('ScorecardCtrl', ['$scope', '$http', '$routeParams',
 				$scope.scorecard = angular.copy(data);
 				$scope.loadCourseData();
  			});
+
+		}]);
+
+/* Search */
+ldrbrdCtrls.controller('SearchCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
+
+		}]);
+
+/* Analysis */
+ldrbrdCtrls.controller('AnalysisCtrl', ['$scope', '$http', '$routeParams',
+		function ($scope, $http, $routeParams) {
 
 		}]);
